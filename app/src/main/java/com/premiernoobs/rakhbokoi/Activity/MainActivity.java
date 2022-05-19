fc5c771fb9e48522a63a2c1bc15b5c47295c4654
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-05-20 05:25:29 +0600

    Activities

---
+++ app/src/main/java/com/premiernoobs/rakhbokoi/Activity/MainActivity.java
@@ -0,0 +1,41 @@
+package com.premiernoobs.rakhbokoi.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.premiernoobs.rakhbokoi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // on screen touch editText focus off
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
+    }
+
+}
--- app/src/main/java/com/premiernoobs/rakhbokoi/Activity/SplashActivity.java
+++ app/src/main/java/com/premiernoobs/rakhbokoi/Activity/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.premiernoobs.rakhbokoi;
+package com.premiernoobs.rakhbokoi.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.premiernoobs.rakhbokoi.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
