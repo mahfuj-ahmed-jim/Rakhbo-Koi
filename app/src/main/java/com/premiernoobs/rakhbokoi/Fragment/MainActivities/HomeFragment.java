977716002115b2aa8926b025718cf11f1d6f7457
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2022-05-22 12:31:51 +0600

    Search Button

--- app/src/main/java/com/premiernoobs/rakhbokoi/Fragment/MainActivities/HomeFragment.java
+++ app/src/main/java/com/premiernoobs/rakhbokoi/Fragment/MainActivities/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,12 +12,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +30,8 @@ import com.premiernoobs.rakhbokoi.Class.Firebase.FirebaseDatabaseClass;
 import com.premiernoobs.rakhbokoi.R;
 import com.premiernoobs.rakhbokoi.Room.LocalUser;
 import com.premiernoobs.rakhbokoi.Room.MainDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeFragment extends Fragment {
 
@@ -41,6 +46,12 @@ public class HomeFragment extends Fragment {
 
     // textView
     private TextView nameTextView, carNumberTextView;
+
+    // imageView
+    private CircleImageView profileImage;
+
+    // editText
+    private TextInputEditText locationEditText;
 
     // firebase
     private DatabaseReference userReference;
@@ -103,6 +114,15 @@ public class HomeFragment extends Fragment {
         // initialize
         init(view);
 
+        // editText on focus change
+        locationEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean isFocus) {
+                changeEditTextString(locationEditText, isFocus);
+            }
+        });
+        // editText on focus change
+
         return view;
     }
 
@@ -130,6 +150,11 @@ public class HomeFragment extends Fragment {
                         User user = dataSnapshot.getValue(User.class);
                         nameTextView.setText("Hey"+user.getName()+"!");
                         carNumberTextView.setText("Car Number : "+user.getCarNumber());
+                        // set image
+                        profileImage.setBackground(null);
+                        Glide.with(getContext())
+                                .load(R.drawable.b2)
+                                .into(profileImage);
                     }
 
                 }
@@ -144,6 +169,18 @@ public class HomeFragment extends Fragment {
 
     }
     // firebase
+
+    // change view
+    private void changeEditTextString(View view, boolean hasFocus){
+
+        if(hasFocus){
+            view.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.edittext_string));
+        }else{
+            view.setBackground(ContextCompat.getDrawable(getActivity().getApplicationContext(), R.drawable.edittext_string2));
+        }
+
+    }
+    // change view
 
     // room database
     private LocalUser getDataFromRoomDatabase(){
@@ -167,6 +204,12 @@ public class HomeFragment extends Fragment {
         // textView
         nameTextView = view.findViewById(R.id.textViewId_name);
         carNumberTextView = view.findViewById(R.id.textViewId_carNumber);
+
+        // imageView
+        profileImage = view.findViewById(R.id.circleImageView);
+
+        // editText
+        locationEditText = view.findViewById(R.id.editTextId_location);
 
         // firebase
         userReference = FirebaseDatabase.getInstance().getReference(new FirebaseDatabaseClass().getUser());
